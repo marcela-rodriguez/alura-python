470edab9065f78bd5d1d769c9560dcade194be3a
Marce <marce735@gmail.com>
2023-10-06 11:14:54 -0500

    utilizabdo ciclo for y range

--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -1,27 +1,28 @@
 print("Adivina el numero")
 
 numero_secreto = 55
-total_intentos = 3
-intento = 1
+#total_intentos = 3
+#intento = 1
 
-while total_intentos >= intento:       
-        print(f"intentos {intento} de {total_intentos}")
-        intento += 1  
+#while total_intentos >= intento:       
+for intento in range(1,4):
+            print(f"intentos {intento} de {3}")
+            #intento += 1  
 
-        entrada = int(input("Digita un numero: "))
-        print("El numero que dijistaste es: ", entrada)
+            entrada = int(input("Digita un numero: "))
+            print("El numero que dijistaste es: ", entrada)
 
-        acerto = numero_secreto == entrada
+            acerto = numero_secreto == entrada
 
-        mayor = entrada > numero_secreto
-        menor = entrada < numero_secreto
+            mayor = entrada > numero_secreto
+            menor = entrada < numero_secreto
 
-        if acerto:
-         print("¡Has acertado!")
-        else:
-            if mayor:
-                print("El numero que dijitaste es mayor")
-            elif menor:
-                print("El numero que dijistate es menor")
-        
+            if acerto:
+                print("¡Has acertado!")
+            else:
+                    if mayor:
+                        print("El numero que dijitaste es mayor")
+                    elif menor:
+                        print("El numero que dijistate es menor")
+                
 print("El juego a concluido")
