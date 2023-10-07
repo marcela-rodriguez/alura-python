a9e2bf8c37b395bc455689e7e5bab0d1523ba39c
Marce <marce735@gmail.com>
2023-10-06 22:11:55 -0500

    mejorando el codigo y usando break para ronper el cliclo y continue para seguir la proxima iteracion

--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -1,28 +1,31 @@
 print("Adivina el numero")
 
 numero_secreto = 55
-#total_intentos = 3
-#intento = 1
+total_intentos = 3
 
-#while total_intentos >= intento:       
-for intento in range(1,4):
-            print(f"intentos {intento} de {3}")
-            #intento += 1  
+     
+for intento in range(1,total_intentos +1):
+        print(f"intentos {intento} de {total_intentos}")
+              
+        entrada = int(input("Digita un numero: "))
+        if entrada < 1 or entrada >100:
+             print("Digite un numero mayor que 0 y menor o igual a 100.")
+             continue
+        
+        print("El numero que dijistaste es: ", entrada)
 
-            entrada = int(input("Digita un numero: "))
-            print("El numero que dijistaste es: ", entrada)
+        acerto = numero_secreto == entrada
 
-            acerto = numero_secreto == entrada
+        mayor = entrada > numero_secreto
+        menor = entrada < numero_secreto
 
-            mayor = entrada > numero_secreto
-            menor = entrada < numero_secreto
-
-            if acerto:
-                print("¡Has acertado!")
-            else:
-                    if mayor:
-                        print("El numero que dijitaste es mayor")
-                    elif menor:
-                        print("El numero que dijistate es menor")
+        if acerto:
+            print("¡Has acertado!")
+            break
+        else:
+            if mayor:
+                print("El numero que dijitaste es mayor")
+            elif menor:
+                print("El numero que dijistate es menor")
                 
 print("El juego a concluido")
