57a3aaa8d5b415a0dcbd5ff000d6a8388023f8e9
Marce <marce735@gmail.com>
2023-10-09 16:31:57 -0500

    abs se utiliza para obtener el valor absoluto de un numero o una operacion

--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -1,11 +1,21 @@
 import random
 print("Adivina el numero")
+print("Elige el nivel de dificultad")
+print(" (1)novato (2) intermedio (3) avanzado")
+
+nivel1=int(input("Nivel: "))
+if nivel1 ==1:
+    total_intentos=20
+elif nivel1==2:
+     total_intentos=10
+else:
+     total_intentos=5
 
 #numero_secreto = round(random.random()*100)
 numero_secreto = random.randint(1,100)
-total_intentos = 3
+print(numero_secreto)
+puntos=1000
 
-     
 for intento in range(1,total_intentos +1):
         print(f"intentos {intento} de {total_intentos}")
               
@@ -22,12 +32,13 @@ for intento in range(1,total_intentos +1):
         menor = entrada < numero_secreto
 
         if acerto:
-            print("¡Has acertado!")
+            print(f"¡Has acertado!tu puntaje es {puntos}")
             break
         else:
             if mayor:
                 print("El numero que dijitaste es mayor")
             elif menor:
                 print("El numero que dijistate es menor")
-                
+            puntos_perdidos = abs(numero_secreto - entrada)
+            puntos= puntos - puntos_perdidos    
 print("El juego a concluido")
--- prueba.py
+++ prueba.py
@@ -1,5 +1,4 @@
 for contador in range(1,11,3):
     print(contador)
 
-    "$ {:7.1f}".format(1000.12)
-"$ {:07.2f}".format(4.11)
+
